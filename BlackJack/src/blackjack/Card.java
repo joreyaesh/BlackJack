561853e80636fe7575f4bc217252daf2fc26ad87
Josh Whaley <josh@whaleymail.net>
2013-03-29 11:35:36 -0400

    Card/Deck Update
    Added content to the Card and Deck classes

--- BlackJack/src/blackjack/Card.java
+++ BlackJack/src/blackjack/Card.java
@@ -1,5 +1,50 @@
 package blackjack;
 
-public class Card {
-
+public Class Card{
+  
+  private int faceValue = 0;
+  private String faceName, suit = "";
+  
+  public void Card(int faceValue, int suitValue){
+    this.faceValue = faceValue;
+    switch(faceValue){
+      case 1:  faceName = "Ace";
+               break;
+      case 11: faceName = "Jack";
+               break;
+      case 12: faceName = "Queen";
+               break;
+      case 13: faceName = "King";
+               break;
+      default: faceName = Integer.toString(faceValue);
+    }
+    switch(suitValueInt){
+      case 1:  suit = "Hearts";
+               break;
+      case 2:  suit = "Diamonds";
+               break;
+      case 3:  suit = "Spades";
+               break;
+      case 4:  suit = "Clubs";
+               break;
+      default: suit = "Invalid Suit";
+               break; 
+    }
+    
+  }
+  
+  
+  public int getFaceValue(){
+    return faceValue;
+  }
+  
+  public String getFaceName(){
+    return faceName;
+  }
+  
+  public String getSuit(){
+    return suit;
+  }
+  
+  
 }
--- BlackJack/src/blackjack/Deck.java
+++ BlackJack/src/blackjack/Deck.java
@@ -1,5 +1,28 @@
 package blackjack;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public Class Deck{
+	
+	private List cards = new ArrayList(52);
+	
+	public void Deck(){
+		for(int i = 1; i<=4; i++){
+		// 4 suits
+		  for int i2 = 1; i2<=13; i2++{
+		    Card card = new Card(i2, i); // i2 is the face value, i is suit value
+		    cards.add(card);
+		  }
+		}
+		shuffle(cards);
+	}
+	
+	public void shuffle(ArrayList cards){
+		// shuffle cards with the shuffle function built into Collections
+	  Collections.shuffle(cards);
+	}
+	
+	
+	
 }
