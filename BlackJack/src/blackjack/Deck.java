db41c87793c2fd6fa00ffde4f9330cc18f844d02
Whaley <whale1jc@cps-pe402-28485.cps.cmich.edu>
2013-04-01 13:49:53 -0400

    Updated Player class and cleaned up Deck class

--- BlackJack/src/blackjack/Deck.java
+++ BlackJack/src/blackjack/Deck.java
@@ -2,82 +2,32 @@ package blackjack;
 
 import java.util.ArrayList;
 import java.util.Collections;
-<<<<<<< HEAD
 import java.util.*;
 
 public class Deck{
-	
-<<<<<<< HEAD
-	public static void main(String[] args){
-		Deck deck = new Deck();
-		for(int i = 0; i<deck.getLength(); i++){
-			System.out.println(deck.cards.get(i).getFaceName() + " of " + deck.cards.get(i).getSuit());
-		}
-	}
-	
-<<<<<<< HEAD
-=======
-	int playerScore = 0;
-	int dealerScore = 0;
->>>>>>> 3/29/13 Allyseia1
-	private List cards = new ArrayList(52);
-=======
-import java.util.List;
-import java.util.Scanner;
 
-public class Deck{
-	
-	private int playerScore = 0;
-	private int dealerScore = 0;
-	
-	private List cards = new ArrayList<Card>(52);
-	String hit;
->>>>>>> 3/29/13 Allyseia3
-	
-	Scanner kb = new Scanner(System.in);
-	
-	
-	public void Deck(){
-=======
-	private int getLength() {
-		// TODO Auto-generated method stub
-		return length;
-	}
+	private static List<Card> cards = new ArrayList<Card>(52);
 
-	private int length = 0;
-	private List<Card> cards = new ArrayList<Card>(52);
-	
 	public Deck(){
->>>>>>> Fixed Card and Deck Classes
 		for(int i = 1; i<=4; i++){
-		// 4 suits
-		  for (int i2 = 1; i2<=13; i2++){
-		    Card card = new Card(i2, i); // i2 is the face value, i is suit value
-		    cards.add(card);
-		    length++;
-		  }
+			// 4 suits
+			for(int i2 = 1; i2<=13; i2++){
+				Card card = new Card(i2, i); // i2 is the face value, i is suit value
+				cards.add(card);
+			}
 		}
 		shuffle(cards);
 	}
-	
-<<<<<<< HEAD
+
 	public void shuffle(List<Card> cards){
-=======
-	public void shuffle(ArrayList cards)
-	{
->>>>>>> 3/29/13 Allyseia3
 		// shuffle cards with the shuffle function built into Collections
-	  Collections.shuffle(cards);
-
+		Collections.shuffle(cards);
 	}
 
-	
-	//if ((dealerScore != 21) && (playerScore != 21))
-	//{
-		//System.out.println("Would you like to hit? (Y/N)");
-		//hit = kb.readLine();
-		//hit = hit.toUpperCase();
-		
+	public static Card giveCard() {
+		Card card = cards.remove(0);
+		return card;
 	}
-	
-	
+
+
+}
--- BlackJack/src/blackjack/Player.java
+++ BlackJack/src/blackjack/Player.java
@@ -1,13 +1,82 @@
 package blackjack;
 
-public class Player {
+import java.util.ArrayList;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+
+public class Player{
+	
+	
+	private String name;
+	private int wallet, bet, total = 0;
+	private ArrayList<Card> hand;
+	
+	
+	public Player(){
 		
-
 	}
-
+	
+	public Player(String name, int wallet){
+		setName(name);
+		setWallet(wallet);
+		hand = new ArrayList<Card>();
+	}
+	
+	private void setBet(int bet){
+		this.bet = bet;
+	}
+	
+	public int getBet(){
+		return bet;
+	}
+	
+	private void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	private void setWallet(int wallet){
+		this.wallet = wallet;
+	}
+	
+	public int getWallet(){
+		return wallet;
+	}
+	
+	private void setTotal(int total){
+		this.total = total;
+	}
+	
+	public int getTotal(){
+		return total;
+	}
+	
+	public void playHand(int bet){
+		clearHand();
+		setBet(bet);
+		requestCards(2);
+	}
+	
+	public void requestCards(int num){
+		for(int i=0; i<num; i++){
+			hand.add(Deck.giveCard());
+		}
+		updateTotal();
+	}
+	
+	private void clearHand(){
+		hand.removeAll(hand);
+		setTotal(0);
+	}
+	
+	private void updateTotal(){
+		setTotal(0);
+		for(int i = 0; i<hand.size(); i++){
+			total += hand.get(i).getFaceValue();
+		}
+		setTotal(total);
+	}
+		
 }
